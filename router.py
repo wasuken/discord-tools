b052355f21b82722ee1a75d80da46ec0137fb7e9
wasuken <wevorence@gmail.com>
2024-09-08 11:16:58 +0900

    更新された場合のみ通知するように

--- router.py
+++ router.py
@@ -1,6 +1,6 @@
 import json
 from urllib.request import Request, urlopen
-from common import post_discord, read_config
+from common import post_discord_if_not_same, read_config
 
 if __name__ == "__main__":
     user_config = read_config()
@@ -10,4 +10,4 @@ if __name__ == "__main__":
         lines = [x for x in [y.split(' ')[3] for y in f.readlines()] if len(x) > 2]
 
     linesStr = '\n'.join(lines)
-    post_discord("現在IPを貸してる端末一覧：" + '\n' +  linesStr, webhook_url)
+    post_discord_if_not_same("現在IPを貸してる端末一覧：" + '\n' +  linesStr, webhook_url)
